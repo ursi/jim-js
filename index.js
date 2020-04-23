ce1dd519bea3a890e2ba4f837f7baa2cf70748ec
Mason Mackaman <masondeanm@aol.com>
2020-04-23 19:48:56 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const
+	functions = {},
+	tasks = {},
+	taskResults = {};
+
+let nextTask;
+
+Object.defineProperties(Object.prototype, {
+	'__jim': {
+		set({type, name, args}) {
+			switch (type) {
+				case `function`:
+					this.return = functions[name](...Array.from(args));
+					break;
+				case `prime task`:
+					nextTask = {name, args};
+					break;
+				case `task result`:
+					this.result = taskResults[name];
+					break;
+			}
+		}
+	},
+});
+
+const setTimeoutOld = setTimeout;
+
+globalThis.setTimeout = (callback, time, ...args) => {
+	if (time === -1) {
+		const
+			{ name
+			, args
+			} = nextTask;
+
+		tasks[name](...Array.from(args))
+			.then(result => {
+				taskResults[name] = result;
+				callback();
+			});
+	} else {
+		return setTimeoutOld(callback, time, ...args);
+	}
+}
+
+function regFunction(name, func) {
+	functions[name] = func;
+}
+
+function regTask(name, func) {
+	tasks[name] = func;
+}
+
+module.exports = {
+	regFunction,
+	regTask,
+};
